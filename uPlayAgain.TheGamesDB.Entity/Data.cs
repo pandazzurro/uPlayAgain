61c68ada34eb381b43f895b2ef98a0d2bd1e13ed
pandazzurro <andreatosato@email.it>
2016-01-10 14:23:23 +0100

    Inizio Http

---
+++ uPlayAgain.TheGamesDB.Entity/Data.cs
@@ -0,0 +1,847 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace uPlayAgain.TheGamesDB.Entity
+{
+    /// <remarks/>
+    [XmlTypeAttribute(AnonymousType = true)]
+    [XmlRootAttribute(Namespace = "", IsNullable = false)]
+    public partial class Data
+    {
+
+        private string baseImgUrlField;
+
+        private DataGame gameField;
+
+        /// <remarks/>
+        public string baseImgUrl
+        {
+            get
+            {
+                return this.baseImgUrlField;
+            }
+            set
+            {
+                this.baseImgUrlField = value;
+            }
+        }
+
+        /// <remarks/>
+        public DataGame Game
+        {
+            get
+            {
+                return this.gameField;
+            }
+            set
+            {
+                this.gameField = value;
+            }
+        }
+
+        public byte[] DowloadedFrontImage { get; set; }
+    }
+
+    /// <remarks/>
+    [XmlTypeAttribute(AnonymousType = true)]
+    public partial class DataGame
+    {
+
+        private int idField;
+
+        private string gameTitleField;
+
+        private int platformIdField;
+
+        private string platformField;
+
+        private string releaseDateField;
+
+        private string overviewField;
+
+        private string eSRBField;
+
+        private DataGameGenres genresField;
+
+        private string playersField;
+
+        private string coopField;
+
+        private string youtubeField;
+
+        private string publisherField;
+
+        private string developerField;
+
+        private decimal ratingField;
+
+        private DataGameSimilar similarField;
+
+        private DataGameImages imagesField;
+
+        /// <remarks/>
+        public int id
+        {
+            get
+            {
+                return this.idField;
+            }
+            set
+            {
+                this.idField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string GameTitle
+        {
+            get
+            {
+                return this.gameTitleField;
+            }
+            set
+            {
+                this.gameTitleField = value;
+            }
+        }
+
+        /// <remarks/>
+        public int PlatformId
+        {
+            get
+            {
+                return this.platformIdField;
+            }
+            set
+            {
+                this.platformIdField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string Platform
+        {
+            get
+            {
+                return this.platformField;
+            }
+            set
+            {
+                this.platformField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string ReleaseDate
+        {
+            get
+            {
+                return this.releaseDateField;
+            }
+            set
+            {
+                this.releaseDateField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string Overview
+        {
+            get
+            {
+                return this.overviewField;
+            }
+            set
+            {
+                this.overviewField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string ESRB
+        {
+            get
+            {
+                return this.eSRBField;
+            }
+            set
+            {
+                this.eSRBField = value;
+            }
+        }
+
+        /// <remarks/>
+        public DataGameGenres Genres
+        {
+            get
+            {
+                return this.genresField;
+            }
+            set
+            {
+                this.genresField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string Players
+        {
+            get
+            {
+                return this.playersField;
+            }
+            set
+            {
+                this.playersField = value;
+            }
+        }
+
+        /// <remarks/>
+        [XmlElementAttribute("Co-op")]
+        public string Coop
+        {
+            get
+            {
+                return this.coopField;
+            }
+            set
+            {
+                this.coopField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string Youtube
+        {
+            get
+            {
+                return this.youtubeField;
+            }
+            set
+            {
+                this.youtubeField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string Publisher
+        {
+            get
+            {
+                return this.publisherField;
+            }
+            set
+            {
+                this.publisherField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string Developer
+        {
+            get
+            {
+                return this.developerField;
+            }
+            set
+            {
+                this.developerField = value;
+            }
+        }
+
+        /// <remarks/>
+        public decimal Rating
+        {
+            get
+            {
+                return this.ratingField;
+            }
+            set
+            {
+                this.ratingField = value;
+            }
+        }
+
+        /// <remarks/>
+        public DataGameSimilar Similar
+        {
+            get
+            {
+                return this.similarField;
+            }
+            set
+            {
+                this.similarField = value;
+            }
+        }
+
+        /// <remarks/>
+        public DataGameImages Images
+        {
+            get
+            {
+                return this.imagesField;
+            }
+            set
+            {
+                this.imagesField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [XmlTypeAttribute(AnonymousType = true)]
+    public partial class DataGameGenres
+    {
+
+        private string genreField;
+
+        /// <remarks/>
+        public string genre
+        {
+            get
+            {
+                return this.genreField;
+            }
+            set
+            {
+                this.genreField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [XmlTypeAttribute(AnonymousType = true)]
+    public partial class DataGameSimilar
+    {
+
+        private int similarCountField;
+
+        private DataGameSimilarGame[] gameField;
+
+        /// <remarks/>
+        public int SimilarCount
+        {
+            get
+            {
+                return this.similarCountField;
+            }
+            set
+            {
+                this.similarCountField = value;
+            }
+        }
+
+        /// <remarks/>
+        [XmlElementAttribute("Game")]
+        public DataGameSimilarGame[] Game
+        {
+            get
+            {
+                return this.gameField;
+            }
+            set
+            {
+                this.gameField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [XmlTypeAttribute(AnonymousType = true)]
+    public partial class DataGameSimilarGame
+    {
+
+        private int idField;
+
+        private int platformIdField;
+
+        /// <remarks/>
+        public int id
+        {
+            get
+            {
+                return this.idField;
+            }
+            set
+            {
+                this.idField = value;
+            }
+        }
+
+        /// <remarks/>
+        public int PlatformId
+        {
+            get
+            {
+                return this.platformIdField;
+            }
+            set
+            {
+                this.platformIdField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [XmlTypeAttribute(AnonymousType = true)]
+    public partial class DataGameImages
+    {
+
+        private DataGameImagesFanart[] fanartField;
+
+        private DataGameImagesBoxart[] boxartField;
+
+        private DataGameImagesBanner[] bannerField;
+
+        private DataGameImagesScreenshot screenshotField;
+
+        private DataGameImagesClearlogo clearlogoField;
+
+        /// <remarks/>
+        [XmlElementAttribute("fanart")]
+        public DataGameImagesFanart[] fanart
+        {
+            get
+            {
+                return this.fanartField;
+            }
+            set
+            {
+                this.fanartField = value;
+            }
+        }
+
+        /// <remarks/>
+        [XmlElementAttribute("boxart")]
+        public DataGameImagesBoxart[] boxart
+        {
+            get
+            {
+                return this.boxartField;
+            }
+            set
+            {
+                this.boxartField = value;
+            }
+        }
+
+        /// <remarks/>
+        [XmlElementAttribute("banner")]
+        public DataGameImagesBanner[] banner
+        {
+            get
+            {
+                return this.bannerField;
+            }
+            set
+            {
+                this.bannerField = value;
+            }
+        }
+
+        /// <remarks/>
+        public DataGameImagesScreenshot screenshot
+        {
+            get
+            {
+                return this.screenshotField;
+            }
+            set
+            {
+                this.screenshotField = value;
+            }
+        }
+
+        /// <remarks/>
+        public DataGameImagesClearlogo clearlogo
+        {
+            get
+            {
+                return this.clearlogoField;
+            }
+            set
+            {
+                this.clearlogoField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [XmlTypeAttribute(AnonymousType = true)]
+    public partial class DataGameImagesFanart
+    {
+
+        private DataGameImagesFanartOriginal originalField;
+
+        private string thumbField;
+
+        /// <remarks/>
+        public DataGameImagesFanartOriginal original
+        {
+            get
+            {
+                return this.originalField;
+            }
+            set
+            {
+                this.originalField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string thumb
+        {
+            get
+            {
+                return this.thumbField;
+            }
+            set
+            {
+                this.thumbField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [XmlTypeAttribute(AnonymousType = true)]
+    public partial class DataGameImagesFanartOriginal
+    {
+
+        private int widthField;
+
+        private int heightField;
+
+        private string valueField;
+
+        /// <remarks/>
+        [XmlAttributeAttribute()]
+        public int width
+        {
+            get
+            {
+                return this.widthField;
+            }
+            set
+            {
+                this.widthField = value;
+            }
+        }
+
+        /// <remarks/>
+        [XmlAttributeAttribute()]
+        public int height
+        {
+            get
+            {
+                return this.heightField;
+            }
+            set
+            {
+                this.heightField = value;
+            }
+        }
+
+        /// <remarks/>
+        [XmlTextAttribute()]
+        public string Value
+        {
+            get
+            {
+                return this.valueField;
+            }
+            set
+            {
+                this.valueField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [XmlTypeAttribute(AnonymousType = true)]
+    public partial class DataGameImagesBoxart
+    {
+
+        private string sideField;
+
+        private int widthField;
+
+        private int heightField;
+
+        private string thumbField;
+
+        private string valueField;
+
+        /// <remarks/>
+        [XmlAttributeAttribute()]
+        public string side
+        {
+            get
+            {
+                return this.sideField;
+            }
+            set
+            {
+                this.sideField = value;
+            }
+        }
+
+        /// <remarks/>
+        [XmlAttributeAttribute()]
+        public int width
+        {
+            get
+            {
+                return this.widthField;
+            }
+            set
+            {
+                this.widthField = value;
+            }
+        }
+
+        /// <remarks/>
+        [XmlAttributeAttribute()]
+        public int height
+        {
+            get
+            {
+                return this.heightField;
+            }
+            set
+            {
+                this.heightField = value;
+            }
+        }
+
+        /// <remarks/>
+        [XmlAttributeAttribute()]
+        public string thumb
+        {
+            get
+            {
+                return this.thumbField;
+            }
+            set
+            {
+                this.thumbField = value;
+            }
+        }
+
+        /// <remarks/>
+        [XmlTextAttribute()]
+        public string Value
+        {
+            get
+            {
+                return this.valueField;
+            }
+            set
+            {
+                this.valueField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [XmlTypeAttribute(AnonymousType = true)]
+    public partial class DataGameImagesBanner
+    {
+
+        private int widthField;
+
+        private int heightField;
+
+        private string valueField;
+
+        /// <remarks/>
+        [XmlAttributeAttribute()]
+        public int width
+        {
+            get
+            {
+                return this.widthField;
+            }
+            set
+            {
+                this.widthField = value;
+            }
+        }
+
+        /// <remarks/>
+        [XmlAttributeAttribute()]
+        public int height
+        {
+            get
+            {
+                return this.heightField;
+            }
+            set
+            {
+                this.heightField = value;
+            }
+        }
+
+        /// <remarks/>
+        [XmlTextAttribute()]
+        public string Value
+        {
+            get
+            {
+                return this.valueField;
+            }
+            set
+            {
+                this.valueField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [XmlTypeAttribute(AnonymousType = true)]
+    public partial class DataGameImagesScreenshot
+    {
+
+        private DataGameImagesScreenshotOriginal originalField;
+
+        private string thumbField;
+
+        /// <remarks/>
+        public DataGameImagesScreenshotOriginal original
+        {
+            get
+            {
+                return this.originalField;
+            }
+            set
+            {
+                this.originalField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string thumb
+        {
+            get
+            {
+                return this.thumbField;
+            }
+            set
+            {
+                this.thumbField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [XmlTypeAttribute(AnonymousType = true)]
+    public partial class DataGameImagesScreenshotOriginal
+    {
+
+        private int widthField;
+
+        private int heightField;
+
+        private string valueField;
+
+        /// <remarks/>
+        [XmlAttributeAttribute()]
+        public int width
+        {
+            get
+            {
+                return this.widthField;
+            }
+            set
+            {
+                this.widthField = value;
+            }
+        }
+
+        /// <remarks/>
+        [XmlAttributeAttribute()]
+        public int height
+        {
+            get
+            {
+                return this.heightField;
+            }
+            set
+            {
+                this.heightField = value;
+            }
+        }
+
+        /// <remarks/>
+        [XmlTextAttribute()]
+        public string Value
+        {
+            get
+            {
+                return this.valueField;
+            }
+            set
+            {
+                this.valueField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [XmlTypeAttribute(AnonymousType = true)]
+    public partial class DataGameImagesClearlogo
+    {
+
+        private int widthField;
+
+        private int heightField;
+
+        private string valueField;
+
+        /// <remarks/>
+        [XmlAttributeAttribute()]
+        public int width
+        {
+            get
+            {
+                return this.widthField;
+            }
+            set
+            {
+                this.widthField = value;
+            }
+        }
+
+        /// <remarks/>
+        [XmlAttributeAttribute()]
+        public int height
+        {
+            get
+            {
+                return this.heightField;
+            }
+            set
+            {
+                this.heightField = value;
+            }
+        }
+
+        /// <remarks/>
+        [XmlTextAttribute()]
+        public string Value
+        {
+            get
+            {
+                return this.valueField;
+            }
+            set
+            {
+                this.valueField = value;
+            }
+        }
+    }
+}
